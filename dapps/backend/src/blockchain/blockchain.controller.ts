7ae9be38c46a10a31ab909b4dbe96515a2ab1f40
Fandy Achmad Ardipraja <saintprave@gmail.com>
2026-01-17 21:47:00 +0700

    fix blockchain controller,service

--- dapps/backend/src/blockchain/blockchain.controller.ts
+++ dapps/backend/src/blockchain/blockchain.controller.ts
@@ -1,33 +1,23 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { BlockchainService } from './blockchain.service';
+import { Controller, Get, Post, Body } from "@nestjs/common";
+import { BlockchainService } from "./blockchain.service";
+import { GetEventsDto } from "./dto/get-events.dto";
 
-@Controller('blockchain')
+@Controller("blockchain")
 export class BlockchainController {
   constructor(private readonly blockchainService: BlockchainService) {}
 
-  @Get()
-  getInfo() {
-    return {
-      message: 'Blockchain API',
-      endpoints: {
-        getValue: 'GET /blockchain/value',
-        getEvents: 'POST /blockchain/events',
-      },
-    };
-  }
-
-  @Get('value')
+  // GET /blockchain/value
+  @Get("value")
   async getValue() {
     return this.blockchainService.getLatestValue();
   }
 
-  @Post('events')
-  async getEvents(@Body() body: { fromBlock: string; toBlock?: string }) {
-    const fromBlock = BigInt(body.fromBlock);
-    const toBlock = body.toBlock === 'latest' || !body.toBlock 
-      ? 'latest' 
-      : BigInt(body.toBlock);
-
-    return this.blockchainService.getValueUpdatedEvents(fromBlock, toBlock);
+  // POST /blockchain/events
+  @Post("events")
+  async getEvents(@Body() body: GetEventsDto) {
+    return this.blockchainService.getValueUpdatedEvents(
+      BigInt(body.fromBlock),
+      body.toBlock ? BigInt(body.toBlock) : "latest"
+    );
   }
 }
